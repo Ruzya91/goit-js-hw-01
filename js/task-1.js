082fa547656933ee1e62275a3a926a75702271da
Ruzanna <pogosian.ruz@gmail.com>
2024-12-23 20:43:01 +0100

    add script

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
+  return `"You ordered ${quantity} droids worth ${
     quantity * pricePerDroid
-  } credits!`;
+  } credits!"`;
 }
